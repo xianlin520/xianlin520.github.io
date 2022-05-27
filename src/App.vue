1c96a721f4b9864f936f87cba03e8c46a02d2d11
XianLin <wanganning5201314@163.com>
2022-05-27 16:53:48 +0800

    提交Vue2项目工程文件v1.0

--- src/App.vue
+++ src/App.vue
@@ -8,25 +8,20 @@
 <style lang="less">
 body {
   // 设置最小宽度, 以免出现布局混乱
-  //min-width: 1800px;
-  height: 100%;
+  min-width: 1800px;
   margin: 0;
   padding: 0;
   border: 0;
 }
-
 #app {
-  height: 100%;
-  margin: 0;
-  padding: 0;
-
   // 设置字体
   font-family: Avenir, Helvetica, Arial, sans-serif;
   // 让字体更平滑, 抗锯齿
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   // 内容居中
-  //text-align: center;
+  text-align: center;
   // 顶部不留白
+  margin-top: 0;
 }
 </style>
